331a3503b672ceb7e54af84a0aed1d4a773b54fe
gmitsolution3 <gmitsolution3@gmail.com>
2026-04-07 18:14:38 +0600

    add missing components

--- components/InstructorDashboard/ActionCells/AllCategoryActionCell.tsx
+++ components/InstructorDashboard/ActionCells/AllCategoryActionCell.tsx
@@ -10,7 +10,7 @@ import {
 import { Eye, Edit, MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import ViewCategoryModal from "@/components/InstructorDashboard/modals/ViewCategoryModal";
-import EditCategoryModal from "@/components/InstructorDashboard/modals/EditCategoryModal";
+import EditCategoryModal from "@/components/InstructorDashboard/modals/EditCategoryModal"; 
 
 export default function AllCategoryActionCell({
   category,
